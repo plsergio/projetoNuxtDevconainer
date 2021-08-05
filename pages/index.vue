50475f1de092da43aa38c7128957071088c439ce
Paulo Sergio <t318789@tjdft.jus.br>
2021-08-05 19:29:10 -0300

    subindo o codigo

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Monitoramento PJE</h1>
+      <v-data-table
+        :headers="headers"
+        :items="lista"
+        :items-per-page="10"
+        class="elevation-1"
+      ></v-data-table>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      lista: [],
+      headers: [
+          { text: 'Titulo', value: 'dsTitulo' },
+        ],
+      }
+  },
+  mounted () {
+    this.carregar()
+  },
+  methods: {
+    carregar (params) {
+      this.$axios.get('https://pjeportal-desenv.apps.tjdft.jus.br/monitoramento/consulta').then(response => {
+        this.lista = response.data.results
+      })
+    },
+  }
+}
+</script>
